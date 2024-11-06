ea2cf9f8d6c96ed8d402e0392327a8bdaf6e9df0
sofiia koval <kovalsofiia1@gmail.com>
2024-11-07 01:23:01 +0200

    feat: page structure

---
+++ app/(tabs)/recomendations.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Recommendations = () => {
+    return (
+        <View>
+            <Text>Recommendations</Text>
+        </View>
+    )
+}
+
+export default Recommendations
+
+const styles = StyleSheet.create({})
--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
   return (
     <View className="flex flex-1 items-center justify-center bg-blue"   >
       <Text className="text-3xl" >Hello Sofiia 1 .</Text>
-      <Link href="/profile" style={{ color: 'blue', textDecorationLine: 'underline' }}>Go to profile</Link>
+      <Link href="/home" style={{ color: 'blue', textDecorationLine: 'underline' }}>Go to profile</Link>
     </View>
   );
 }
